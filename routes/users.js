e9f94bf2c525f79b6137e4655701baf804a3962a
Anusha Payidiparthi <Anusha.Payidiparthi@berkadia.com>
2020-05-09 16:28:05 +0530

    created new folder using express generator

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  let mydbSchema = mongoose.Schema({
-    name: String,
-    email: String
-  });
-
-  let dataToSave = new mydbSchema({
-    name: 'demo',
-    email: 'demo@demo.com'
-  });
-
-  dataToSave.save(function (err) {
-    if (err) throw err;
-    console.log('User created successfully.');
-  });
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
